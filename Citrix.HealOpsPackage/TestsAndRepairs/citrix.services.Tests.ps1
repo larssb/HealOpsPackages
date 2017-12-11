07f6f6584c0c3993558d79d13c2ebd1e604b7b15
Lars Slouborg Bengtsson <LABE@danskespil.dk>
2017-12-11 15:20:43 +0100

    Bump version because of implemented failedTestResult variable and passedTestResult variable.

--- Citrix.HealOpsPackage/TestsAndRepairs/citrix.services.Tests.ps1
+++ Citrix.HealOpsPackage/TestsAndRepairs/citrix.services.Tests.ps1
@@ -3,26 +3,21 @@ Describe "citrix.services" {
         - Tests that all Citrix services is running
     #>
     It "All citrix services should be running" {
-        # Get all the Citrix services on the local computer. Uses the Displayname parameter as this is the best bet of getting Citrix services.
-        $citrixServices = get-service -DisplayName "*Citrix*"
+        # Get all the Citrix services on the local computer.
+        $citrixServices = get-service -Name "Citrix*"
 
-        if ($null -ne $citrixServices) {
-            foreach ($citrixService in $citrixServices) {
-                # The BNBOOTP svc. stops automatically. By design by Citrix.
-                if ($citrixService.Status -ne "Running" -and $citrixService.Name -ne "BNBOOTP") {
-                    $serviceOkay = 1
-                    break
-                } else {
-                    $serviceOkay = 0
-                }
+        foreach ($citrixService in $citrixServices) {
+            if ($citrixService.Status -ne "Running") {
+                $serviceOkay = 1
+                $global:failedTestResult = 1
+                break
+            } else {
+                $serviceOkay = 0
             }
-        } else {
-            # Either Citrix is not installed on this node or trying to get the Citrix services failed.
-            $serviceOkay = 1
         }
-            
+
         # Determine the result of the test
-        $global:assertionResult = 0
+        $global:passedTestResult = 0
         $serviceOkay | Should Be 0
     }
 }
