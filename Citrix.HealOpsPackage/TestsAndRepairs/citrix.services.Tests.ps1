bcc3cab110064c70bf3945a7f048e1ff593a6c61
Lars Slouborg Bengtsson <LABE@danskespil.dk>
2017-12-11 16:01:41 +0100

    Bugfix was mistankely not included.

--- Citrix.HealOpsPackage/TestsAndRepairs/citrix.services.Tests.ps1
+++ Citrix.HealOpsPackage/TestsAndRepairs/citrix.services.Tests.ps1
@@ -3,19 +3,25 @@ Describe "citrix.services" {
         - Tests that all Citrix services is running
     #>
     It "All citrix services should be running" {
-        # Get all the Citrix services on the local computer.
-        $citrixServices = get-service -Name "Citrix*"
+        # Get all the Citrix services on the local computer. Uses the Displayname parameter as this is the best bet of getting Citrix services.
+        $citrixServices = get-service -DisplayName "*Citrix*"
 
-        foreach ($citrixService in $citrixServices) {
-            if ($citrixService.Status -ne "Running") {
-                $serviceOkay = 1
-                $global:failedTestResult = 1
-                break
-            } else {
-                $serviceOkay = 0
+        if ($null -ne $citrixServices) {
+            foreach ($citrixService in $citrixServices) {
+                # The BNBOOTP svc. stops automatically. By design by Citrix.
+                if ($citrixService.Status -ne "Running" -and $citrixService.Name -ne "BNBOOTP") {
+                    $serviceOkay = 1
+                    $global:failedTestResult = 1                    
+                    break
+                } else {
+                    $serviceOkay = 0
+                }
             }
+        } else {
+            # Either Citrix is not installed on this node or trying to get the Citrix services failed.
+            $serviceOkay = 1
         }
-
+            
         # Determine the result of the test
         $global:passedTestResult = 0
         $serviceOkay | Should Be 0
