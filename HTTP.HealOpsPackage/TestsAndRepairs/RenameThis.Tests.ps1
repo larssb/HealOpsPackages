5f3a3beaea4a583d080ea1a0c8296973a6fb3d8b
Lars Bengtsson <bingchonglars@gmail.com>
2018-06-05 22:20:34 +0200

    Now have a working Vault integrated Pester test in the HTTP HealOps package.

--- HTTP.HealOpsPackage/TestsAndRepairs/RenameThis.Tests.ps1
+++ HTTP.HealOpsPackage/TestsAndRepairs/RenameThis.Tests.ps1
@@ -1,54 +1,49 @@
 ####
 # PREP.
 ####
-[String]$Name = "httphealopspackage"
-# Get the AppRole RoleID of the user with access to read the required secrets.
-try {
-    [String]$VaultRoleID = Read-AppRoleRoleID -EnvironmentVariableName $Name -ErrorAction Stop
-} catch {
-    Write-Error -Exception "Failed to get the RoleID. Failed with > $_"
-}
+[String]$SubsonicSecret = Request-VaultSecret -Name httphealopspackage -SecretPath "subsonicApi"
+Describe 'Subsonic.IbigServices' {
+    # general variables
+    $ibigServicesRootURI = "https://ibigservices.ddns.net";
 
-#
-try {
-    $WrappedSecretID = Get-AppRoleSecretID -RoleName $Name -Wrapped -ErrorAction Stop
-} catch {
-
-}
-
-# Unwrap the SecretID
-try {
-    Get-AppRoleUnwrappedSecretID -RoleName $Name
-} catch {
-
-}
-
-# Login to Vault
-try {
-    Login-AppRole -RoleID $VaultRoleID -SecretID
-} catch {
-
-}
-
-# Get the required secrets
-try {
-
-} catch {
-
-}
-
-
-Describe "name.in.this.format" {
     <#
-        - "Describe what the test does"
-    #>
-    It "NAME_THE_TEST" {
-        #
-        'TEST_CODE_HERE'
-        $global:failedTestResult =
+        - Test the health of the Subsonic music service
 
-        # Determine the result of the test
-        $global:passedTestResult =
-        $ | Should .....
+        Runs on Tomcat
+    #>
+    It "The status of Subsonic should be ok" {
+        # The subsonic health endpoint URI
+        $subsonicHealthURI = "$ibigServicesRootURI/subsonic/rest/ping.view"
+
+        # Prep. query parameters for the call to Subsonics health endpoint.
+        $requestQuery = @{}
+        $requestQuery.c = "subsonicHealth" # c for client application
+        $requestQuery.f = "json" # f for format
+        $requestQuery.s = Get-Random # s for salt
+
+        # Get the password matching the user
+        $password = $SubsonicSecret
+        if ($IsMacOS -eq $true -or $IsLinux -eq $true) {
+            # MacOS or Linux - use the MD5()
+            $saltedPass = md5 -q -s $password$($requestQuery.s)
+        }
+
+        $requestQuery.t = $saltedPass # t for token.
+        $requestQuery.u = "subsonicApi"; # u for user.
+        $requestQuery.v = "1.15.0"; # v for version
+
+        try {
+            # Call the Subsonic health endpoint
+            $requestResult = Invoke-WebRequest $subsonicHealthURI -Method Get -Body $requestQuery;
+        } catch {
+            $status = "not_ok"
+        }
+
+        $json = $requestResult.Content;
+        $requestResult = ConvertFrom-Json $json;
+        $status = $requestResult.'subsonic-response'.status;
+
+        # Determine the result of this Pester test.
+        $status | Should Be "ok";
     }
 }
